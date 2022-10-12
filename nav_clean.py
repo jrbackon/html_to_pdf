f7e0369c947764d1ca1e13d66287a53ff5d22698
Jacob Backon <jrbackon@gmail.com>
2022-10-12 12:38:09 -0400

    Created a second function to remove individual lines as this was not working in the original function as intended.

--- nav_clean.py
+++ nav_clean.py
@@ -4,16 +4,12 @@ import os
 directory = input("What is the path of the folder containing the .html files you would like to clean? ")
 
 def rem_chunk(source):
-    begin_table = """ cellpadding="0" """
+    begin_table = "<table"
     end = "<hr"
     end_table = "</table>"
-    cap_hr = "<HR>"
-    link = "<a href="
     begin_block = "<blockquote>"
     end_block = "</blockquote>"
-    cpyrgt = "Babson College All Rights Reserved"
-    stamp = "<!--webbot bot="
-
+    
     flag = False
     with open(source, 'r+', encoding='utf8', errors='ignore') as f:
         page = f.readlines()
@@ -29,18 +25,32 @@ def rem_chunk(source):
                 f.write(line)
             else:
                 pass
+        f.truncate()
+    return None
 
+def rem_line(source):
+    end = "<hr"
+    cap_hr = "<HR>"
+    link = "<a href="
+    cpyrgt = "Babson College All Rights Reserved"
+    stamp = "<!--webbot bot="
+    end_table = "</table>"
+    end_block = "</blockquote>"
+    with open(source, 'r+', encoding='utf8', errors='ignore') as f:
+        page = f.readlines()
         f.seek(0)
         for line in page:
-            if end in line or cap_hr in line or link in line or cpyrgt in line or stamp in line or end_table in line:
+            if end in line or cap_hr in line or link in line or cpyrgt in line or stamp in line or end_table in line or end_block in line:
                 pass
             else:
                f.write(line) 
         f.truncate() # this ensures there is nothing written to the file after the for loop completes.
+    return None
 
 for root, subdirectories, files in os.walk(directory): # this for loop walks through all directories in the specified folder, and runs the ablrule function on all .html files
     for file in files:
         source = os.path.join(root, file)
         if ".html" in source:
             rem_chunk(source)
+            rem_line(source)
             
